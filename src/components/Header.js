c878b5a5a372c7b72a7a22b230ac614078ef18ff
Graham <grey.ham.acre@gmail.com>
2018-02-18 16:45:55 +0300

    refactor to functional component with semantic html

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <a href="https://www.freecodecamp.com/"><img id="logo" alt="freecodecamp logo" src='https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg' /></a>
-      </div>
-    );
-  }
-}
-export default Header;
+export default () => (
+  <header className="header">
+    <a href="https://www.freecodecamp.com/">
+      <img
+        id="logo"
+        alt="freecodecamp logo"
+        src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg"
+      />
+    </a>
+  </header>
+);
